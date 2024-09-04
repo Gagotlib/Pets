b98471231e55fd4b451e82983cff4dad1e3976e2
Gabriel Gotlib <gagotlib@gmail.com>
2024-09-04 15:41:55 +0200

    first commit

---
+++ front/src/App.jsx
@@ -0,0 +1,30 @@
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import Navbar from './components/navbar/Navbar'
+import Home from './views/home/Home'
+import Login from './views/login/Login'
+import ErrorPage from './views/ErrorPage/ErrorPage'
+import Detail from './views/detail/Detail'
+import Register from './views/register/Register'
+import Publishfound from './views/publishfound/Publishfound'
+import Publishlost from './views/publishlost/Publishlost'
+
+function App() {
+	return (
+		<>
+			<Navbar />
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/login' element={<Login />} />
+				<Route path='/detail' element={<Detail />} />
+				<Route path='/register' element={<Register />} />
+				<Route path='/publishfound' element={<Publishfound />} />
+				<Route path='/publishlost' element={<Publishlost />} />
+				<Route path='/*' element={<ErrorPage />} />
+			</Routes>
+
+		</>
+	)
+}
+
+export default App
